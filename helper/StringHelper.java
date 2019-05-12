bfe0affe5d622789add587a1cb96ad3aa7a01f0a
nidhaldring <36518218+nidhaldring@users.noreply.github.com>
2019-05-12 02:34:04 +0100

    Add files via upload

--- helper/StringHelper.java
+++ helper/StringHelper.java
@@ -1,1 +1,40 @@
 
+package helper;
+
+
+import static java.io.File.separator;
+
+public class StringHelper{
+
+
+	public static String reverse(String s){
+
+		String res = "";
+		int len = s.length();
+		for(int i=1;i<=len;++i){
+
+			res += s.charAt(len-i);
+		}
+		return res;
+	}
+
+	public static int getNumberOfLines(String s){
+
+		int nb = 0;
+		for(int i =0;i<s.length();++i){
+
+			if(s.charAt(i) == '\n'){
+				++nb;
+			}
+		}
+
+		return nb;
+	}
+
+	public static String toPath(String a,String b){
+
+		return a + separator + b;
+	}
+
+
+}
